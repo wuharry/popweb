fdf40acaeecdb67b966cc21b5f2058b133dae931
Harvey.Wu <whw880218@pgtalk.com>
2023-06-19 18:01:21 +0800

    littlePage排版修正去除log

--- src/pages/home/component/littlepage3/index.js
+++ src/pages/home/component/littlepage3/index.js
@@ -19,11 +19,9 @@ const LittlePage3 = () => {
   const imgeup = useRef(null);
   const imgedown = useRef(null);
   useEffect(() => {
-    // setScrollY(0);
     const handleScroll = () => {
       // 開始時圖片有偏差,當瀏覽器視窗底部等於分頁3時偏差修正
-      const windowBotton = window.scrollY + window.innerHeight;//當瀏覽器視窗底部
-      console.log((window.scrollY / 10)-200);
+      // const windowBotton = window.scrollY + window.innerHeight;//當瀏覽器視窗底部
       setScrollY((window.scrollY / 10)-200);
     };
     window.addEventListener("scroll", handleScroll);
