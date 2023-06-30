eaa86e46140cd9d9e789db0d3e40e6445e396391
Harvey.Wu <whw880218@pgtalk.com>
2023-06-30 16:21:31 +0800

    修改滑動效果,去除img的Ref

--- src/pages/home/component/littlepage2/index.js
+++ src/pages/home/component/littlepage2/index.js
@@ -2,8 +2,8 @@ import styles from "./style.module.scss";
 import React, { useCallback, useEffect, useRef } from "react";
 const LittlePage2 = () => {
   const dot = useRef([]);
-  const imges = useRef([]);
-  const currentNum = 0;
+  const img =useRef(null);
+  let currentNum = 0;
   useEffect(() => {
     // 初始時新增黑點
     const { innerWidth } = window;
@@ -21,11 +21,7 @@ const LittlePage2 = () => {
       dot.current.push(node);
     }
   }, []);
-  const setImges = useCallback((img) => {
-    if (img && !imges.current.includes(img)) {
-      imges.current.push(img);
-    }
-  }, []);
+  // 圖片ref
   const handleClick = (refName) => {
     currentNum = refName;
     // 处理值变化事件的逻辑
@@ -34,6 +30,7 @@ const LittlePage2 = () => {
       element.classList.remove(styles.active);
     });
     map[refName - 1].classList.add(styles.active);
+    img.current.classList.add(styles.slide);
   };
   return (
     <>
@@ -42,13 +39,13 @@ const LittlePage2 = () => {
           <h2>Take your event to the next level</h2>
           <p>Elevate your invite with our powerful features</p>
         </div>
-        <div className={styles.feature_wrapper}>
-          <div className={styles.feature} ref={setImges}>
+        <div className={styles.feature_wrapper} ref={img}>
+          <div className={styles.feature}>
             <img src="/images/feature_fullscreen.svg" alt="SVG Image" />
             <h3>Beautiful Full-screen Invitations</h3>
             <p>Designed to look good on the web, phones and tablets.</p>
           </div>
-          <div className={styles.feature} ref={setImges}>
+          <div className={styles.feature}>
             <img src="/images/feature_customize.svg" alt="SVG Image" />
             <h3>Customize with Photos and Videos</h3>
             <p>
@@ -56,47 +53,47 @@ const LittlePage2 = () => {
               galleries.
             </p>
           </div>
-          <div className={styles.feature} ref={setImges}>
+          <div className={styles.feature}>
             <img src="/images/feature_send.svg" alt="SVG Image" />
             <h3>Send Invitations via Email or Text</h3>
             <p>Choose your guests and we’ll send the invitation for you.</p>
           </div>
-          <div className={styles.feature} ref={setImges}>
+          <div className={styles.feature}>
             <img src="/images/feature_rsvp.svg" alt="SVG Image" />
             <h3>Robust RSVP Management Tools</h3>
             <p>See who has responded, added plus-ones and opened invites.</p>
           </div>
           {/* 滑動點 */}
-          <div className={styles.dots_container}>
-            <span
-              className={styles.dot}
-              ref={setDot}
-              onClick={() => {
-                handleClick(1);
-              }}
-            ></span>
-            <span
-              className={styles.dot}
-              ref={setDot}
-              onClick={() => {
-                handleClick(2);
-              }}
-            ></span>
-            <span
-              className={styles.dot}
-              ref={setDot}
-              onClick={() => {
-                handleClick(3);
-              }}
-            ></span>
-            <span
-              className={styles.dot}
-              ref={setDot}
-              onClick={() => {
-                handleClick(4);
-              }}
-            ></span>
-          </div>
+        </div>
+        <div className={styles.dots_container}>
+          <span
+            className={styles.dot}
+            ref={setDot}
+            onClick={() => {
+              handleClick(1);
+            }}
+          ></span>
+          <span
+            className={styles.dot}
+            ref={setDot}
+            onClick={() => {
+              handleClick(2);
+            }}
+          ></span>
+          <span
+            className={styles.dot}
+            ref={setDot}
+            onClick={() => {
+              handleClick(3);
+            }}
+          ></span>
+          <span
+            className={styles.dot}
+            ref={setDot}
+            onClick={() => {
+              handleClick(4);
+            }}
+          ></span>
         </div>
       </div>
     </>
