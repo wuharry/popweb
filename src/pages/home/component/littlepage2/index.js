c6d15c1dac49513457c2bd375145bbe707c7c872
Harvey.Wu <whw880218@pgtalk.com>
2023-06-30 16:43:25 +0800

    RWF 轉動圖完成

--- src/pages/home/component/littlepage2/index.js
+++ src/pages/home/component/littlepage2/index.js
@@ -2,15 +2,31 @@ import styles from "./style.module.scss";
 import React, { useCallback, useEffect, useRef } from "react";
 const LittlePage2 = () => {
   const dot = useRef([]);
-  const img =useRef(null);
+  const imgRef = useRef(null);
   let currentNum = 0;
   useEffect(() => {
+    const handleScroll = () => {
+      const scrollLeft = imgRef.current.scrollLeft;
+      // console.log(Math.floor(scrollLeft));
+      const refName = Math.floor(scrollLeft / 375);
+      const map = dot.current;
+      map.forEach((element) => {
+        element.classList.remove(styles.active);
+      });
+
+      map[refName].classList.add(styles.active);
+    };
     // 初始時新增黑點
     const { innerWidth } = window;
+    imgRef.current.addEventListener("scroll", handleScroll);
     const deviceWidth = 600;
     if (innerWidth <= deviceWidth) {
       dot.current[0].classList.add(styles.active);
     }
+
+    return () => {
+      imgRef.current.removeEventListener("scroll", handleScroll);
+    };
     //
   }, []);
 
@@ -22,16 +38,6 @@ const LittlePage2 = () => {
     }
   }, []);
   // 圖片ref
-  const handleClick = (refName) => {
-    currentNum = refName;
-    // 处理值变化事件的逻辑
-    const map = dot.current;
-    map.forEach((element) => {
-      element.classList.remove(styles.active);
-    });
-    map[refName - 1].classList.add(styles.active);
-    img.current.classList.add(styles.slide);
-  };
   return (
     <>
       <div className={styles.contain}>
@@ -39,7 +45,7 @@ const LittlePage2 = () => {
           <h2>Take your event to the next level</h2>
           <p>Elevate your invite with our powerful features</p>
         </div>
-        <div className={styles.feature_wrapper} ref={img}>
+        <div className={styles.feature_wrapper} ref={imgRef}>
           <div className={styles.feature}>
             <img src="/images/feature_fullscreen.svg" alt="SVG Image" />
             <h3>Beautiful Full-screen Invitations</h3>
@@ -66,34 +72,10 @@ const LittlePage2 = () => {
           {/* 滑動點 */}
         </div>
         <div className={styles.dots_container}>
-          <span
-            className={styles.dot}
-            ref={setDot}
-            onClick={() => {
-              handleClick(1);
-            }}
-          ></span>
-          <span
-            className={styles.dot}
-            ref={setDot}
-            onClick={() => {
-              handleClick(2);
-            }}
-          ></span>
-          <span
-            className={styles.dot}
-            ref={setDot}
-            onClick={() => {
-              handleClick(3);
-            }}
-          ></span>
-          <span
-            className={styles.dot}
-            ref={setDot}
-            onClick={() => {
-              handleClick(4);
-            }}
-          ></span>
+          <span className={styles.dot} ref={setDot}></span>
+          <span className={styles.dot} ref={setDot}></span>
+          <span className={styles.dot} ref={setDot}></span>
+          <span className={styles.dot} ref={setDot}></span>
         </div>
       </div>
     </>
